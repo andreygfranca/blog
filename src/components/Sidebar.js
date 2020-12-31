e0c13c37a8fe92ea04e50d9565107535c2a13c34
Andrey Franca <andrey.franca@zgsolucoes.com.br>
2020-12-31 15:06:51 -0300

    feat: adding sidebar style

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react';
 class Sidebar extends Component {
     render() {
         return (
-            <div>
-                This is the sidebar
+            <div className="sidebar">
+                <a href="#">About</a>
+                <a href="#">Services</a>
+                <a href="#">Clients</a>
+                <a href="#">Contact</a>
             </div>
         );
     }
